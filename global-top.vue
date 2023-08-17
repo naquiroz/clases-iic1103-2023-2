0b5aba97da01bb39b8ae2564d98691f363d94acb
Nicolas <nicolasquirozpa@gmail.com>
2023-08-17 00:41:08 -0400

    fix: show dynamic url

--- global-top.vue
+++ global-top.vue
@@ -1,7 +1,8 @@
 <template>
-  <a :href="BASE_URL" class="absolute top-0 left-0 dark:text-zinc-500 text-zinc-300 p-2 text-xs"> {{"<--"}} Volver a todas las clases</a>
+  <a v-if="showURL" :href="URL" class="absolute top-0 left-0 dark:text-zinc-500 text-zinc-300 p-2 text-xs"> {{"<-  Volver a todas las clases"}}</a>
 </template>
 
 <script setup lang="ts">
-const BASE_URL = import.meta.env.BASE_URL || ""
+const URL = window.location.origin
+const showURL = window.location.pathname.includes("clase_")
 </script>
