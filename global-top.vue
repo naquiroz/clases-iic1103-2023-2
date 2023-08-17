460420e53c070a9a8d8ee8328e98bae41364242b
Nicolas <nicolasquirozpa@gmail.com>
2023-08-17 00:36:48 -0400

    fix: add global back

---
+++ global-top.vue
@@ -0,0 +1,7 @@
+<template>
+  <a :href="BASE_URL" class="absolute top-0 left-0 dark:text-zinc-500 text-zinc-300 p-2 text-xs"> {{"<--"}} Volver a todas las clases</a>
+</template>
+
+<script setup lang="ts">
+const BASE_URL = import.meta.env.BASE_URL || ""
+</script>
